354cf3d8a8f0da98453165ba6b577a001d2aac37
eng-aac <aldo.castillo.13@gmail.com>
2026-03-06 11:51:55 -0300

    Edit 404 github

--- post-build.js
+++ post-build.js
@@ -4,10 +4,29 @@ const path = require('path');
 const distPath = path.join( __dirname, 'dist/portfolio-daac' );
 const indexFile = path.join( distPath, 'index.html' );
 const errorFile = path.join( distPath, '404.html' );
+const noJekyllFile = path.join( distPath, '.nojekyll' );
 
 try {
-  if ( fs.existsSync( indexFile ) ) fs.copyFileSync( indexFile, errorFile );
-  else console.error( 'Error: No se encontró index.html en ' + distPath );
-} catch ( error) {
-  console.error( 'Error al copiar el archivo:', error );
+
+  let html = fs.readFileSync(indexFile, 'utf8');
+  const recoveryScript = `
+    <script>
+      ( function() {
+        var redirect = sessionStorage.redirect;
+        delete sessionStorage.redirect;
+
+        if ( redirect && redirect != location.href ) {
+          history.replaceState( null, null, redirect );
+        }
+      } )();
+    </script>`
+  ;
+  html = html.replace( '</body>', recoveryScript + '</body>' );
+  fs.writeFileSync( errorFile, html );
+  
+  fs.writeFileSync( noJekyllFile, '' );
+  
+  console.log( 'Post-build completado: 404.html y .nojekyll creados.' );
+} catch ( error ) {
+  console.error( 'Error en postbuild:', error );
 }
